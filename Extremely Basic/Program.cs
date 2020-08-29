d28b43a69c953afbfb6853c54c49fa19e0cdf2c2
Mehedy-Hasan <codewithmehedy@gmail.com>
2020-08-29 11:31:42 +0600

    Test For Commit

--- Extremely Basic/Program.cs
+++ Extremely Basic/Program.cs
@@ -6,40 +6,6 @@ namespace Extremely_Basic
     {
         static void Main(string[] args)
         {
-            double a, b, c, d, avg;
-            string[] values = Console.ReadLine().Split(' ');
-            a = double.Parse(values[0]);
-            b = double.Parse(values[1]);
-            c = double.Parse(values[2]);
-            d = double.Parse(values[3]);
-            avg = (a * 2 + b * 3 + c * 4 + d * 1) / (2 + 3 + 4 + 1);
-            Console.WriteLine("Media: " + Math.Round(avg,1));
-            if (avg >= 7.0d)
-            {
-                Console.WriteLine("Aluno aprovado.");
-            }
-            if (avg < 5.0d)
-            {
-                Console.WriteLine("Aluno reprovado.");
-            }
-            if (avg >= 5.0d && avg <= 6.9d)
-            {
-                Console.WriteLine("Aluno em exame.");
-                Console.Write("Nota do exame: ");
-                double e = double.Parse(Console.ReadLine());
-                avg = (avg + e) / 2;
-                if (avg > 5.0d)
-                {
-                    Console.WriteLine("Aluno aprovado.");
-                }
-                if (avg < 5.0d)
-                {
-                    Console.WriteLine("Aluno reprovado.");
-                }
-                Console.WriteLine("Media final: " + Math.Round(avg,1));
-            }
-
-            Console.ReadKey();
         }
     }
 }
